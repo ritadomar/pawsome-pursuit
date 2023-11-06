4e0b9fc9124c2a53a04523a3760de971a95bb452
Carolina Rodrigues <rodriguesbcarolina@gmail.com>
2023-11-06 21:26:16 +0000

    changed logic around starting the game

--- js/script.js
+++ js/script.js
@@ -1,44 +1,39 @@
 // delays the JS from running straight away!
 // imagine the website has very heavy images. we have a safekeep below to allow the website to load first and the JS to run next
 window.onload = function () {
-  // const startButton = document.querySelector('#start-button');
   const playerSelector = document.querySelectorAll('.player-selector');
   const restartButton = document.getElementById('restart-button');
 
-  // check later if we can write the code in a separate way (not inside the for each)
+  let type;
+  let game;
+
   playerSelector.forEach(button => {
     button.addEventListener('click', () => {
       type = button.getAttribute('id');
-
-      console.log(type);
-      const game = new Game(type);
+      game = new Game(type);
 
       game.start();
-
-      window.addEventListener('keydown', event => {
-        event.preventDefault();
-
-        switch (event.key) {
-          case 'ArrowUp':
-            game.player.directionY = -1;
-            break;
-          case 'ArrowDown':
-            game.player.directionY = 1;
-            break;
-          case 'ArrowLeft':
-            game.player.directionX = -1;
-            break;
-          case 'ArrowRight':
-            game.player.directionX = 1;
-            break;
-        }
-      });
     });
   });
 
-  /* startButton.addEventListener('click', () => {
-    startGame();
-  }); */
+  window.addEventListener('keydown', event => {
+    event.preventDefault();
+
+    switch (event.key) {
+      case 'ArrowUp':
+        game.player.directionY = -1;
+        break;
+      case 'ArrowDown':
+        game.player.directionY = 1;
+        break;
+      case 'ArrowLeft':
+        game.player.directionX = -1;
+        break;
+      case 'ArrowRight':
+        game.player.directionX = 1;
+        break;
+    }
+  });
 
   restartButton.addEventListener('click', () => {
     // reloads the browser
